0b30952aeb4528be27c29216060227b337db7b9e
Gavin18049 <lawsongavin928@gmail.com>
2024-10-26 17:08:21 -0400

    Adding bar and bubble chart

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,22 +1,25 @@
 // Build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    console.log(data);
 
     // get the metadata field
-
+    const metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    const result = metadata.filter(meta => meta.id == sample)[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    const panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key}: ${value}`);
+    });
   });
 }
 
@@ -25,29 +28,59 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    const samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    const result = samples.find(sampleObj => sampleObj.id === sample);
 
     // Get the otu_ids, otu_labels, and sample_values
-
-
+    const sampleValues = result.sample_values.slice(0, 10).reverse(); // Top 10 sample values, reversed for bar chart
+    const otuIds = result.otu_ids.slice(0, 10).reverse(); // Top 10 OTU IDs, reversed for bar chart
+    const otuLabels = result.otu_labels.slice(0, 10).reverse(); // Top 10 OTU labels, reversed for bar chart
+   
     // Build a Bubble Chart
-
+    const bubbleData = [{
+      x: result.otu_ids,
+      y: result.sample_values,
+      text: result.otu_labels,
+      mode: 'markers',
+      marker: {
+        size: result.sample_values,
+        color: result.otu_ids,
+        colorscale: 'Earth'
+      }
+    }];
 
     // Render the Bubble Chart
+    const bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: { title: 'OTU IDs' },
+      yaxis: { title: 'Number of Bacteria' },
+      showlegend: false
+    };
 
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    const barData = [{
+      type: 'bar',
+      x: sampleValues,
+      y: otuIds.map(id => `OTU ${id}`), // Format OTU IDs for display
+      text: otuLabels,
+      orientation: 'h'
+    }];
 
     // Build a Bar Chart
+    const barLayout = {
+      title: 'Top 10 Bacteria Cultures Found',
+      xaxis: { title: 'Number of Bacteria' },
+      yaxis: { title: 'OTU IDs' }
+    };
     // Don't forget to slice and reverse the input data appropriately
 
 
     // Render the Bar Chart
-
+    Plotly.newPlot('bar', barData, barLayout);
   });
 }
 
